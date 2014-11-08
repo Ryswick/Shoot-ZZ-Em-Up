e3bd9d6355003ab0abd5a9a06be63dcd9dc02add
Ryswick <bahamutheroes@yahoo.fr>
2014-11-08 18:39:41 +0100

    Added Game Bases

---
+++ ShootEmUp/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	void Update ()
+	{
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		Move (direction);
+	}
+		
+	void Move (Vector2 direction)
+	{
+		//Screen limits for the player
+		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+		Vector2 pos = transform.position;
+
+		pos += direction  * speed * Time.deltaTime;
+
+		//Ensure that the player will not go out of the screen
+		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
+
+		transform.position = pos;
+	}
+}
