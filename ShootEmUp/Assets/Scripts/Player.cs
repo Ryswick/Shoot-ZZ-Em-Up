3be5fb86ef135cb36f081fd5e90edaf557686f5e
Ryswick <bahamutheroes@yahoo.fr>
2014-11-11 16:01:48 +0100

    Player Movement
    - Added "Player" script, which enable the player to move without going
    out of the screen.

--- ShootEmUp/Assets/Scripts/Player.cs
+++ ShootEmUp/Assets/Scripts/Player.cs
@@ -7,18 +7,23 @@ public class Player : MonoBehaviour {
 
 	void Update ()
 	{
-		float x = Input.GetAxisRaw ("Horizontal");
-		float y = Input.GetAxisRaw ("Vertical");
-		Vector2 direction = new Vector2 (x, y).normalized;
-
-		Move (direction);
+		Move ();
 	}
 		
-	void Move (Vector2 direction)
+	void Move ()
 	{
+		//Get player entries for movement
+		Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		//Put camera coordinates in World Coordinates
+		Vector2 wrld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+		//Calculate half size of the player sprite
+		Vector2 half_size = (gameObject.GetComponent<SpriteRenderer>().bounds.size)/2;
+
 		//Screen limits for the player
-		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+		Vector2 min = new Vector2(-wrld.x + half_size.x, -wrld.y + half_size.y);
+		Vector2 max = new Vector2(wrld.x/3, wrld.y - half_size.y);
 
 		Vector2 pos = transform.position;
 
