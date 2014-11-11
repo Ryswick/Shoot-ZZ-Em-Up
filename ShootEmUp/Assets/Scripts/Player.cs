978fad896cd77eb6af168a2b910d0b49e2c3cdff
Ryswick <bahamutheroes@yahoo.fr>
2014-11-11 20:53:46 +0100

    Classes & Projectile Instantiation
    -Added many of the necessary classes for the game scene and the menu
    -Enable the player to instantiate projectiles when hitting the Z Key

--- ShootEmUp/Assets/Scripts/Player.cs
+++ ShootEmUp/Assets/Scripts/Player.cs
@@ -1,38 +1,113 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Player : MonoBehaviour {
+public class Player : Ship {
 
-	public float speed;
+	private int nbLife;
+	private int nbBomb;
+	private int shootDamage;
+	private float upgradeRatio;
+	private Bombe bombe;
+	private List<Bonus> bonusList;
+	public Projectile projectile;
+	private bool isShooting;
+	private bool isDead;
+
+	void Start ()
+	{
+		nbLife = 3;
+		nbBomb = 1;
+		orbDropped = 0;
+		isShooting = false;
+		isDead = false;
+
+		bonusList = new List<Bonus>();
+
+		Live();
+	}
 
 	void Update ()
 	{
-		Move ();
+		if(!isDead)
+		{
+			Move ();
+
+			if(Input.GetKey(KeyCode.Z) && !isShooting)
+			{
+				isShooting = true;
+				StartCoroutine(Shoot(shootSpeed));
+			}
+			if(Input.GetKeyUp(KeyCode.Z))
+				isShooting = false;
+		}
 	}
-		
+
+	//Allow the player to move on the game screen
 	void Move ()
 	{
 		//Get player entries for movement
 		Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
 		//Put camera coordinates in World Coordinates
-		Vector2 wrld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+		Vector3 wrld = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
 		//Calculate half size of the player sprite
-		Vector2 half_size = (gameObject.GetComponent<SpriteRenderer>().bounds.size)/2;
+		Vector2 halfSize = (gameObject.GetComponent<SpriteRenderer>().bounds.size)/2;
 
 		//Screen limits for the player
-		Vector2 min = new Vector2(-wrld.x + half_size.x, -wrld.y + half_size.y);
-		Vector2 max = new Vector2(wrld.x/3, wrld.y - half_size.y);
+		Vector2 min = new Vector2(-wrld.x + halfSize.x, -wrld.y + halfSize.y);
+		Vector2 max = new Vector2(wrld.x/3, wrld.y - halfSize.y);
 
 		Vector2 pos = transform.position;
 
-		pos += direction  * speed * Time.deltaTime;
+		pos += direction * speed * Time.deltaTime;
 
 		//Ensure that the player will not go out of the screen
 		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
 		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
 
 		transform.position = pos;
+	}
+
+	void Death()
+	{
+		isDead = true;
+		isShooting = false;
+		nbLife--;
+
+		// Game Over if nbLife < 0
+	}
+
+	//Initialize the player data each time he comes back to life
+	void Live()
+	{
+		HP = 1;
+		speed = 5;
+		
+		shootDamage = 1;
+		upgradeRatio = 0;
+
+		bonusList.Clear();
+
+		//Put camera coordinates in World Coordinates
+		Vector3 wrld = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+		//Calculate half size of the player sprite
+		float halfxSize = (gameObject.GetComponent<SpriteRenderer>().bounds.size.x)/2;
+
+		gameObject.transform.position = new Vector3(-wrld.x/3 + halfxSize, -wrld.y/2, 0);
+
+		isDead = false;
+	}
+
+	IEnumerator Shoot(float shootSpeed)
+	{
+		while(isShooting)
+		{
+			Instantiate(projectile, gameObject.rigidbody2D.position, new Quaternion());
+
+			yield return new WaitForSeconds(shootSpeed);
+		}
 	}
 }
